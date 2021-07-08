b0ad6c6dfc689d7b14bd1aeb9fe9b768ba39b307
Obiora Hillary <obicj97@gmail.com>
2021-07-07 18:03:41 -0700

    init

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,41 +4,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet">
+    <link rel="stylesheet" href="{{url('assets/bootstrap/css/bootstrap.css')}}">
+    <script src="{{url('assets/bootstrap/js/bootstrap.js')}}"></script>
     <title>authentication</title>
 </head>
-<body class="bg-gray-600">
-    <nav class="">
-        <ul class="">
-            <li>
-                <a href="">Home</a>
-            </li>
-            <li>
-                <a href= "{{route('dashboard')}}">Dashboard</a>
-            </li>
-        </ul>
-
-        <ul>
-            @auth
-            <form action="{{route('logout')}}" method="post">
-                @csrf
-                <button type="submit">Logout</button>
-            </form>
-            @endauth
-
-            @guest
-            <li>
-                <a href="{{route('login')}}">Login</a>
-            </li>
-
-            <li>
-                <a href="{{route('register')}}">Register</a>
-            </li>
-            @endguest
-                
-                        
-        </ul>
-    </nav>
+<div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-success">
+    <img class="img-thumbnail img-fluid " src="thumbnail.jpg" alt="" style="width:50px; height:50px;">
+    <a class="navbar-brand" href=""><h3>Simple Post Web</h3></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+    
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home
+                            <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('dashboard')}}">Dashboard</a>
+                    </li>                  
+        
+                    @auth
+                    <form action="{{route('logout')}}" method="post">
+                        @csrf
+                        <button type="submit">Logout</button>
+                    </form>
+                    @endauth
+        
+                    @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('login')}}">Login</a>
+                    </li>
+        
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('register')}}">Register</a>
+                    </li>
+                    @endguest                                  
+                </ul>
+            </div>
+        </nav>
    @yield('content')
 </body>
 </html>
